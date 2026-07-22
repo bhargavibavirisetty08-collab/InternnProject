81c67009594a1e967da046badf5ed90944fe5c02
bhargavibavirisetty08-collab <bhargavibavirisetty18@gmail.com>
2026-07-22 21:11:08 +0530

    Add files via upload
    updating the repoes

--- InternProject/src/main/java/com/example/InternProject/Repo/OrderRepo.java
+++ InternProject/src/main/java/com/example/InternProject/Repo/OrderRepo.java
@@ -9,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface OrderRepo extends JpaRepository<Order , Integer> {
-    List<Order> findByStatus(OrderStatus status);
+    List<Order> findByStatusIn(List<OrderStatus> statuses);
 }
