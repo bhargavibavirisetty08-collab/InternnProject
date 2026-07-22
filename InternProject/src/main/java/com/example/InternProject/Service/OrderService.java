bf069e1d5dd0ae5dd241b5581711be5f9e323ce3
bhargavibavirisetty08-collab <bhargavibavirisetty18@gmail.com>
2026-07-22 21:09:51 +0530

    Add files via upload
    updating the order , portfolio services

--- InternProject/src/main/java/com/example/InternProject/Service/OrderService.java
+++ InternProject/src/main/java/com/example/InternProject/Service/OrderService.java
@@ -2,6 +2,7 @@ package com.example.InternProject.Service;
 
 import com.example.InternProject.Model.*;
 import com.example.InternProject.Repo.OrderRepo;
+import com.example.InternProject.Repo.PortfolioRepo;
 import com.example.InternProject.Repo.TransactionRepo;
 import com.example.InternProject.Repo.UserRepo;
 import jakarta.annotation.PostConstruct;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class OrderService {
@@ -26,18 +28,59 @@ public class OrderService {
     @Autowired
     TransactionRepo transactionRepo; //to add order details in database
 
+    @Autowired
+    PortfolioService portfolioService;
+
+    @Autowired
+    PortfolioRepo portfolioRepo;
+
 //    private PriorityQueue<Order> buyQueue = new PriorityQueue<>(((o1, o2) -> Double.compare(o2.getPrice() , o1.getPrice())));
 //    private PriorityQueue<Order> sellQueue = new PriorityQueue<>(((o1, o2) -> Double.compare(o1.getPrice() , o2.getPrice())));
 
-    private Map<Integer , PriorityQueue<Order>> buyQueues = new HashMap<>();
-    private  Map<Integer , PriorityQueue<Order>> sellQueues = new HashMap<>();
+    private Map<Integer , PriorityQueue<Order>> buyQueues = new ConcurrentHashMap<>();
+    private  Map<Integer , PriorityQueue<Order>> sellQueues = new ConcurrentHashMap<>();
 
-    private PriorityQueue<Order> getBuyQueue (Integer stockId){
-        return buyQueues.computeIfAbsent(stockId , id -> new PriorityQueue<>((o1 , o2) -> Double.compare(o2.getPrice() , o1.getPrice())));
+//    private PriorityQueue<Order> getBuyQueue (Integer stockId){
+//        return buyQueues.computeIfAbsent(stockId , id -> new PriorityQueue<>((o1 , o2) -> Double.compare(o2.getPrice() , o1.getPrice())));
+//    }
+//
+//    private PriorityQueue<Order> getSellQueue (Integer stockId){
+//        return sellQueues.computeIfAbsent(stockId , id->new PriorityQueue<>(((o1, o2) -> Double.compare(o1.getPrice() , o2.getPrice()))));
+//    }
+
+
+    //to check the No two threads are come at same time !
+    private Map<Integer,Object> stockLocks = new ConcurrentHashMap<>();
+    private Object getStockLock(Integer stockId){
+
+        return stockLocks.computeIfAbsent(
+                stockId,
+                id -> new Object()
+        );
     }
 
-    private PriorityQueue<Order> getSellQueue (Integer stockId){
-        return sellQueues.computeIfAbsent(stockId , id->new PriorityQueue<>(((o1, o2) -> Double.compare(o1.getPrice() , o2.getPrice()))));
+    private PriorityQueue<Order> getBuyQueue(Integer stockId) {
+        return buyQueues.computeIfAbsent(stockId, id ->
+                new PriorityQueue<>((o1, o2) -> {
+                    int priceCompare = Double.compare(o2.getPrice(), o1.getPrice());
+                    if (priceCompare != 0) {
+                        return priceCompare;
+                    }
+                    return o1.getCreatedAt().compareTo(o2.getCreatedAt());
+                })
+        );
+    }
+
+    private PriorityQueue<Order> getSellQueue(Integer stockId) {
+        return sellQueues.computeIfAbsent(stockId, id ->
+                new PriorityQueue<>((o1, o2) -> {
+                    int priceCompare = Double.compare(o1.getPrice(), o2.getPrice());
+                    if (priceCompare != 0) {
+                        return priceCompare;
+                    }
+                    return o1.getCreatedAt().compareTo(o2.getCreatedAt());
+                })
+        );
     }
 
     public List<Order> getAllOrders(){
@@ -48,37 +91,74 @@ public class OrderService {
         return orderRepo.findById(id).orElseThrow(() -> new RuntimeException("Order not found"));
     }
 
+    @Transactional
     public Order sellOrder(Order order) {
 
+        // 1. Check seller owns enough shares
+        Portfolio portfolio = portfolioRepo
+                .findByUserAndStock(order.getUser(), order.getStock())
+                .orElseThrow(() -> new RuntimeException("Stock not found in portfolio"));
+
+        int availableShares = portfolio.getQuantity() - portfolio.getLockedQuantity();
+
+        if(order.getQuantity() > availableShares){
+            throw new RuntimeException("Not enough available shares");
+        }
+        // Lock shares
+        portfolio.setLockedQuantity(
+                portfolio.getLockedQuantity() + order.getQuantity()
+        );
+
+        portfolioRepo.save(portfolio);
+
+        // 2. Set order details
         order.setType(Order_type.SELL);
         order.setStatus(OrderStatus.PENDING);
         order.setCreatedAt(LocalDateTime.now());
+        order.setOriginalQuantity(order.getQuantity());
 
-        Order savedOrder =  orderRepo.save(order);
+        // 3. Save order
+        Order savedOrder = orderRepo.save(order);
 
+        // 4. Add to sell queue
         PriorityQueue<Order> queue = getSellQueue(savedOrder.getStock().getId());
         queue.add(savedOrder);
-        matchOrders(savedOrder.getStock().getId());
 
+        // 5. Try matching
+        matchOrders(savedOrder.getStock().getId());
         return savedOrder;
     }
 
+    @Transactional
     public Order buyOrder(Order order) {
+        double requiredAmount = order.getPrice() * order.getQuantity();
+        order.setLockedAmount(requiredAmount);
+
+        User buyer = order.getUser();
+
+        double availableBalance =
+                buyer.getBalance() - buyer.getLockedBalance();
+
+        if(requiredAmount > availableBalance){
+            throw new RuntimeException("Insufficient available balance");
+        }
 
         order.setType(Order_type.BUY);
         order.setStatus(OrderStatus.PENDING);
         order.setCreatedAt(LocalDateTime.now());
+        order.setOriginalQuantity(order.getQuantity());
 
-        double requiredAmount = order.getPrice() * order.getQuantity();
-        User buyer = order.getUser();
-        if(requiredAmount > buyer.getBalance()){
-            throw new RuntimeException("Insufficient balance");
-        }
+        buyer.setLockedBalance(
+                buyer.getLockedBalance() + requiredAmount
+        );
 
-        Order savedOrder =  orderRepo.save(order);
+        userRepo.save(buyer);
+
+        Order savedOrder = orderRepo.save(order);
 
         PriorityQueue<Order> queue = getBuyQueue(savedOrder.getStock().getId());
         queue.add(savedOrder);
+
         matchOrders(savedOrder.getStock().getId());
 
         return savedOrder;
@@ -116,30 +196,55 @@ public class OrderService {
 //        return orderRepo.save(getOrder);
 //    }
 
+    @Transactional
+    public String deleteOrder(int id) {
+        Order order = orderRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
 
-//    public String deleteOrder(int id) {
-//
-//        Order order = orderRepo.findById(id)
-//                .orElseThrow(() -> new RuntimeException("Order not found"));
-//
-//        if (order.getType() == Order_type.BUY) {
-//            buyQueue.remove(order);
-//        } else {
-//            sellQueue.remove(order);
-//        }
-//
-//        orderRepo.delete(order);
-//
-//        return "Deleted Successfully";
-//    }
+        if(order.getStatus() == OrderStatus.COMPLETED){
+            throw new RuntimeException("Completed order cannot be cancelled");
+        }
+
+        // Remove from queue
+        if(order.getType() == Order_type.BUY){
+            PriorityQueue<Order> buyQueue = getBuyQueue(order.getStock().getId());
+            buyQueue.remove(order);
+            // Release locked money
+            User buyer = order.getUser();
+            buyer.setLockedBalance(
+                    buyer.getLockedBalance() - order.getLockedAmount()
+            );
+            userRepo.save(buyer);
+        } else {
+            PriorityQueue<Order> sellQueue =
+                    getSellQueue(order.getStock().getId());
+            sellQueue.remove(order);
+            // Release locked shares
+            Portfolio portfolio =
+                    portfolioRepo.findByUserAndStock(
+                            order.getUser(),
+                            order.getStock()
+                    ).orElseThrow(() -> new RuntimeException("Portfolio not found"));
+
+            portfolio.setLockedQuantity(portfolio.getLockedQuantity() - order.getQuantity());
+            portfolioRepo.save(portfolio);
+        }
+
+        order.setStatus(OrderStatus.CANCELLED);
+        orderRepo.save(order);
+        return "Order Cancelled Successfully";
+    }
 
     @PostConstruct
     public void loadPendingOrders() {
-
-        List<Order> pendingOrders = orderRepo.findByStatus(OrderStatus.PENDING);
+        List<Order> pendingOrders =  orderRepo.findByStatusIn(List.of(
+                        OrderStatus.PENDING,
+                        OrderStatus.PARTIALLY_FILLED));
 
         for (Order order : pendingOrders) {
-
+            if(order.getStock() == null){
+                continue;
+            }
             if (order.getType() == Order_type.BUY) {
                 getBuyQueue(order.getStock().getId()).add(order);
             } else {
@@ -151,15 +256,20 @@ public class OrderService {
 
     @Transactional
     private void matchOrders(Integer stockId) {
-        PriorityQueue<Order> buyQueue = getBuyQueue(stockId);
-        PriorityQueue<Order> sellQueue = getSellQueue(stockId);
 
-        while (!buyQueue.isEmpty() && !sellQueue.isEmpty()) {
-            Order buyOrder = buyQueue.peek();
-            Order sellOrder = sellQueue.peek();
+        synchronized (getStockLock(stockId)) {
 
-            if (buyOrder.getStock().getId().equals(sellOrder.getStock().getId())
-                    && buyOrder.getPrice() >= sellOrder.getPrice()) {
+            PriorityQueue<Order> buyQueue = getBuyQueue(stockId);
+            PriorityQueue<Order> sellQueue = getSellQueue(stockId);
+
+            while (!buyQueue.isEmpty() && !sellQueue.isEmpty()) {
+                Order buyOrder = buyQueue.peek();
+                Order sellOrder = sellQueue.peek();
+
+                // Check price matching
+                if (buyOrder.getPrice() < sellOrder.getPrice()) {
+                    break;
+                }
 
                 int tradedQuantity = Math.min(
                         buyOrder.getQuantity(),
@@ -167,7 +277,10 @@ public class OrderService {
                 );
 
                 double tradeAmount = sellOrder.getPrice() * tradedQuantity;
+                buyOrder.setLockedAmount(buyOrder.getLockedAmount() - tradeAmount);
 
+
+                // Create transaction history
                 Transaction transaction = new Transaction();
 
                 transaction.setBuyer(buyOrder.getUser());
@@ -179,43 +292,69 @@ public class OrderService {
 
                 transactionRepo.save(transaction);
 
+                // Update portfolios
+                portfolioService.updatePortfolio(
+                        buyOrder.getUser(),
+                        sellOrder.getUser(),
+                        buyOrder.getStock(),
+                        tradedQuantity,
+                        sellOrder.getPrice()
+                );
+
+                // Update balances
                 User buyer = buyOrder.getUser();
                 User seller = sellOrder.getUser();
 
-                // Update balance
-                buyer.setBalance(buyer.getBalance() - tradeAmount);
-                seller.setBalance(seller.getBalance() + tradeAmount);
+                buyer.setLockedBalance(
+                        buyer.getLockedBalance() - tradeAmount
+                );
+                buyer.setBalance(
+                        buyer.getBalance() - tradeAmount
+                );
+                seller.setBalance(
+                        seller.getBalance() + tradeAmount
+                );
 
-                // Save users
                 userRepo.save(buyer);
                 userRepo.save(seller);
-                // Update remaining quantity
+
+                // Reduce remaining quantities
                 buyOrder.setQuantity(
                         buyOrder.getQuantity() - tradedQuantity
                 );
+
                 sellOrder.setQuantity(
                         sellOrder.getQuantity() - tradedQuantity
                 );
 
-                // Update status and remove completed orders
+                // Update BUY order status
                 if (buyOrder.getQuantity() == 0) {
                     buyOrder.setStatus(OrderStatus.COMPLETED);
+//                    buyer.setLockedBalance(
+//                            buyer.getLockedBalance() - buyOrder.getLockedAmount()
+//                    );
+                    userRepo.save(buyer);
                     buyQueue.poll();
+                } else {
+                    buyOrder.setStatus(OrderStatus.PARTIALLY_FILLED);
                 }
+
+                // Update SELL order status
                 if (sellOrder.getQuantity() == 0) {
                     sellOrder.setStatus(OrderStatus.COMPLETED);
                     sellQueue.poll();
+                } else {
+                    sellOrder.setStatus(OrderStatus.PARTIALLY_FILLED);
                 }
-
                 // Save updated orders
                 orderRepo.save(buyOrder);
                 orderRepo.save(sellOrder);
 
                 System.out.println("Trade completed");
+                System.out.println("Stock : " + buyOrder.getStock().getSymbol());
                 System.out.println("Quantity : " + tradedQuantity);
+                System.out.println("Price : " + sellOrder.getPrice());
                 System.out.println("Amount : " + tradeAmount);
-            } else {
-                break;
             }
         }
     }
--- InternProject/src/main/java/com/example/InternProject/Service/PortfolioService.java
+++ InternProject/src/main/java/com/example/InternProject/Service/PortfolioService.java
@@ -2,6 +2,8 @@ package com.example.InternProject.Service;
 
 import com.example.InternProject.Model.Order;
 import com.example.InternProject.Model.Portfolio;
+import com.example.InternProject.Model.Stock;
+import com.example.InternProject.Model.User;
 import com.example.InternProject.Repo.OrderRepo;
 import com.example.InternProject.Repo.PortfolioRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +18,44 @@ public class PortfolioService {
 
     public List<Portfolio> getAllPortfolio(){
         return portfolioRepo.findAll();
+    }
+
+    public void updatePortfolio(User buyer , User seller , Stock stock , int Quantity , double price){
+        Portfolio sellerPortfolio = portfolioRepo.findByUserAndStock(seller , stock).orElseThrow(()->new RuntimeException("Stock not Found"));
+        sellerPortfolio.setQuantity(sellerPortfolio.getQuantity() - Quantity);
+
+        sellerPortfolio.setLockedQuantity(
+                sellerPortfolio.getLockedQuantity() - Quantity
+        );
+
+        portfolioRepo.save(sellerPortfolio);
+
+        Portfolio buyerPortfolio = portfolioRepo.findByUserAndStock(buyer , stock).orElse(null);
+
+        if(buyerPortfolio == null){
+            buyerPortfolio = new Portfolio();
+            buyerPortfolio.setUser(buyer);
+            buyerPortfolio.setQuantity(Quantity);
+            buyerPortfolio.setAveragePrice(price);
+            buyerPortfolio.setStock(stock);
+        }
+        else{
+            int oldQuantity = buyerPortfolio.getQuantity();
+            double oldAveragePrice = buyerPortfolio.getAveragePrice();
+
+            int totalQuantity = oldQuantity + Quantity;
+
+            double newAveragePrice =
+                    ((oldQuantity * oldAveragePrice) + (Quantity * price))
+                            / totalQuantity;
+
+            buyerPortfolio.setQuantity(totalQuantity);
+            buyerPortfolio.setAveragePrice(newAveragePrice);
+        }
+        portfolioRepo.save(buyerPortfolio);
+    }
+
+    public Portfolio assignPortfolio(Portfolio portfolio) {
+        return portfolioRepo.save(portfolio);
     }
 }
